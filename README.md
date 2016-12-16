e2f3eec91e284d56e59aa2c419f5719cfd4223d2
Toxagin <toxagin@gmail.com>
2016-12-16 16:47:20 +0300

    implement module github_trending.py whith two functions get_trending_repositories(top_size) get_open_issues_amount(repo_owner, repo_name). Document this module

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # 9_github_trending
+
+## Synopsis
+
+Script helps you to find twenty most popular repositories on github, which were created for the last week. Additionally for the every repositori shows the list of opened issues
+
+## Quick start
+
+ - Install all requirements from file `pip3 install -r requirements.txt`
+ - Run github_trending.py `python3.5 github_trending.py`
+ - **Input**: nothing to need any input
+ - **Output**: List of popular repositories with following information: name, owner, url and issues list (issue title, issue url)
--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,41 @@
-
+import requests
+from datetime import timedelta, date
 
 def get_trending_repositories(top_size):
-    pass
+    search_template = 'https://api.github.com/search/repositories?'\
+    'q=created:>={week_ago}&sort=stars&order=desc'\
+    '&access_token=98a292be015a2a4abf04dea17629c4de1f69db83'
+
+    week_ago = date.today() - timedelta(days=7)
+    response = requests.get(search_template.format(week_ago=week_ago))
+    
+    return response.json()['items'][:top_size]
 
 
 def get_open_issues_amount(repo_owner, repo_name):
-    pass
+    issues_template = 'https://api.github.com/repos/'\
+    '{owner}/{repo}/issues'\
+    '?access_token=98a292be015a2a4abf04dea17629c4de1f69db83'
+
+    response = requests.get(issues_template.format(
+        owner=repo_owner,
+        repo=repo_name
+    ))
+
+    return response.json()
+
 
 if __name__ == '__main__':
-    pass
+    for enum, repo in enumerate(get_trending_repositories(20), start=1):
+        owner = repo['owner']['login']
+        repo_name = repo['name']
+        print('{0}. Repository: {1}'.format(enum, repo_name))
+        print('   Owner: {0}'.format(owner))
+        print('   Url: {0}'.format(repo['html_url']))
+        issues = get_open_issues_amount(owner, repo_name)
+        print('   Opened issues {0}:'.format(len(issues)))
+        for issue in issues:
+            print('    - {0}. ref: {1}'.format(
+                issue['title'],
+                issue['html_url']
+            ))
