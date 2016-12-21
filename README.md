0480c2a0a6071c4627e357a4aeba0a3c04027459
Toxagin <toxagin@gmail.com>
2016-12-21 21:04:25 +0300

    Now script works with single request to api. Magic numbers disappear. Now in request used params instead of write it in url directly

--- README.md
+++ README.md
@@ -9,4 +9,4 @@ Script helps you to find twenty most popular repositories on github, which were 
  - Install all requirements from file `pip3 install -r requirements.txt`
  - Run github_trending.py `python3.5 github_trending.py`
  - **Input**: nothing to need any input
- - **Output**: List of popular repositories with following information: name, owner, url and issues list (issue title, issue url)
+ - **Output**: List of popular repositories with following information: name, owner, url and opend issues amount
--- github_trending.py
+++ github_trending.py
@@ -1,41 +1,32 @@
 import requests
 from datetime import timedelta, date
 
+NUMBER_OF_REPOSITORIES = 20
+DAYS_PER_WEEK = 7
+ACCESS_TOKEN = '98a292be015a2a4abf04dea17629c4de1f69db83'
+
 def get_trending_repositories(top_size):
-    search_template = 'https://api.github.com/search/repositories?'\
-    'q=created:>={week_ago}&sort=stars&order=desc'\
-    '&access_token=98a292be015a2a4abf04dea17629c4de1f69db83'
+    search_url = 'https://api.github.com/search/repositories'
 
-    week_ago = date.today() - timedelta(days=7)
-    response = requests.get(search_template.format(week_ago=week_ago))
-    
-    return response.json()['items'][:top_size]
+    week_ago = date.today() - timedelta(days=DAYS_PER_WEEK)
 
-
-def get_open_issues_amount(repo_owner, repo_name):
-    issues_template = 'https://api.github.com/repos/'\
-    '{owner}/{repo}/issues'\
-    '?access_token=98a292be015a2a4abf04dea17629c4de1f69db83'
-
-    response = requests.get(issues_template.format(
-        owner=repo_owner,
-        repo=repo_name
-    ))
-
-    return response.json()
-
+    params = {
+        'q': 'created:>={week_ago}'.format(week_ago=week_ago),
+        'sort': 'stars',
+        'order': 'desc',
+        'access_token': ACCESS_TOKEN,
+        'per_page': top_size
+    }    
+    response = requests.get(search_url, params=params)
+    return response.json()['items']
 
 if __name__ == '__main__':
-    for enum, repo in enumerate(get_trending_repositories(20), start=1):
+    for enum, repo in enumerate(
+        get_trending_repositories(NUMBER_OF_REPOSITORIES),
+        start=1):
         owner = repo['owner']['login']
         repo_name = repo['name']
         print('{0}. Repository: {1}'.format(enum, repo_name))
         print('   Owner: {0}'.format(owner))
         print('   Url: {0}'.format(repo['html_url']))
-        issues = get_open_issues_amount(owner, repo_name)
-        print('   Opened issues {0}:'.format(len(issues)))
-        for issue in issues:
-            print('    - {0}. ref: {1}'.format(
-                issue['title'],
-                issue['html_url']
-            ))
+        print('   Opened issues {0}:'.format(repo['open_issues_count']))
